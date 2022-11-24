156b3f1034d4239885c60057747dcec147ffe356
Cédric PIERSON <cdric.pierson@gmail.com>
2022-11-24 13:57:22 +0100

    API call AirQuality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import AirQuality from './components/AirQuality';
 
 function App() {
+  const [city, setCity] = useState("");
+  useEffect(()=>{
+    setCity("Strasbourg ")
+  },[])
   return (
     <div>
-      <AirQuality city={city}/>
+      <AirQuality city={city} />
     </div>
   );
 }
--- src/components/AirQuality.jsx
+++ src/components/AirQuality.jsx
@@ -1,13 +1,34 @@
-import React from "react";
-import { Typography } from "@mui/material";
+import React, { useEffect } from "react";
+import { Box, Typography } from "@mui/material";
 
-export default function AirQuality() {
+export default function AirQuality({ city }) {
   const token = "ec08e40ed8653e9069b989b3a6e4bb240d2fbafd";
+
+  useEffect(() => {
+    fetch(`https://api.waqi.info/feed/${city}/?token=${token}`)
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [city]);
+
   return (
-    <div>
-      <Typography variant="h6" href="https://waqi.info/">
-        World Air Quality Index
+    <Box
+      style={{
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <Typography variant="body2">
+        {city}
+        <a
+          href="https://waqi.info/"
+          style={{ textDecoration: "none", color: "#000" }}
+        >
+          World Air Quality Index
+        </a>
       </Typography>
-    </div>
+    </Box>
   );
 }
