6db6a397ebda853c0495a1ca7a9b64ec5ca321af
Geoffrey Hach <je.suis.geoffrey.hach@gmail.com>
2022-11-24 16:49:22 +0100

    fixed theme provider

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { createTheme, ThemeProvider } from "@mui/material";
 import "./App.css";
 import Home from "./pages/Home";
 
+const theme = createTheme({
+    typography: {
+        title: { fontFamily: "Righteous, cursive" },
+    },
+    palette: {
+        white: {
+            main: "#FFFFFF",
+        },
+    },
+});
+
 function App() {
-    return <Home />;
+    return (
+        <ThemeProvider theme={theme}>
+            <Home />
+        </ThemeProvider>
+    );
 }
 
 export default App;
