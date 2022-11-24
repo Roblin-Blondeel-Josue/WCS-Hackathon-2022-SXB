22a216cb520a1f34bb5e71fde9ab554e0ea9c116
Geoffrey Hach <je.suis.geoffrey.hach@gmail.com>
2022-11-24 18:10:35 +0100

    changed axios command

--- src/components/CityHeader.jsx
+++ src/components/CityHeader.jsx
@@ -3,20 +3,17 @@ import axios from "axios";
 import { Box, Typography } from "@mui/material";
 
 function CityHeader({ city }) {
-    const [img, setImg] = useState(
-        "https://images.unsplash.com/photo-1511739001486-6bfe10ce785f?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzODMzMTh8MHwxfHNlYXJjaHwxfHxwYXJpc3xlbnwwfHx8fDE2NjkyOTUwODE&ixlib=rb-4.0.3&q=80"
-    );
+    const [img, setImg] = useState();
+    // "https://images.unsplash.com/photo-1511739001486-6bfe10ce785f?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzODMzMTh8MHwxfHNlYXJjaHwxfHxwYXJpc3xlbnwwfHx8fDE2NjkyOTUwODE&ixlib=rb-4.0.3&q=80"
 
     // const apiKey = "Y0ARhn_ULsZYMBKuqKRgGRen0RPKFSmo6Hq4T-mpTVE";
 
     // useEffect(() => {
     //     axios
     //         .get(
-    //             `https://api.unsplash.com//search/photos?query=${city}+building+architecture&orientation=landscape&per_page=1&client_id=${apiKey}`
+    //             `https://api.unsplash.com//search/photos?query=${city}+buildings&orientation=landscape&per_page=1&client_id=${apiKey}`
     //         )
     //         .then((res) => {
-    //             console.log(res.data);
-    //             console.log(res.data.results[0].urls.regular);
     //             setImg(res.data.results[0].urls.regular);
     //         });
     // }, [city]);
@@ -31,7 +28,7 @@ function CityHeader({ city }) {
                 backgroundPosition: "center",
                 backgroundSize: "cover",
                 minWidth: "100vw",
-                minHeight: "100vh",
+                height: "85vh",
             }}
         >
             <Typography variant="h1" color="white" sx={{ fontWeight: "bold" }}>
