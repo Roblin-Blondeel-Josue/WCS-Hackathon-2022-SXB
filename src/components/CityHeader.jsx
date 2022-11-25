42e7b6b2b64d3a1af2d08166bb1bdd1cb1f815c6
Cédric PIERSON <cdric.pierson@gmail.com>
2022-11-25 11:58:28 +0100

    cleaning code

--- src/components/CityHeader.jsx
+++ src/components/CityHeader.jsx
@@ -4,10 +4,9 @@ import { Box, Stack, Typography } from "@mui/material";
 import GenerateButton from "./GenerateButton";
 
 function CityHeader({ city, setCity, list, setChecked }) {
-    const [img, setImg] = useState(
-        "https://images.unsplash.com/photo-1511739001486-6bfe10ce785f?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzODMzMTh8MHwxfHNlYXJjaHwxfHxwYXJpc3xlbnwwfHx8fDE2NjkyOTUwODE&ixlib=rb-4.0.3&q=80"
-    );
-
+  const [img, setImg] = useState(
+    "https://images.unsplash.com/photo-1511739001486-6bfe10ce785f?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzODMzMTh8MHwxfHNlYXJjaHwxfHxwYXJpc3xlbnwwfHx8fDE2NjkyOTUwODE&ixlib=rb-4.0.3&q=80"
+  );
 
   // const apiKey = "Y0ARhn_ULsZYMBKuqKRgGRen0RPKFSmo6Hq4T-mpTVE";
 
@@ -21,52 +20,44 @@ function CityHeader({ city, setCity, list, setChecked }) {
   //         });
   // }, [city]);
 
-    return (
-        <Box
-            display="flex"
-            alignItems="center"
-            justifyContent="center"
-            sx={{
-                position: "relative",
-                backgroundImage: `url(${img})`,
-                backgroundPosition: "center",
-                backgroundSize: "cover",
-                minWidth: "100vw",
-                height: "85vh",
-            }}
-        >
-            <Box
-                sx={{
-                    position: "absolute",
-                    top: "1rem",
-                    right: { sm: "1rem" },
-                }}
-            >
-                <GenerateButton
-                    list={list}
-                    setCity={setCity}
-                    setChecked={setChecked}
-                    isTryAgain
-                />
-            </Box>
-            <Stack alignItems="center" justifyContent="center">
-                <Typography
-                    variant="h1"
-                    color="white.main"
-                    sx={{ fontWeight: "bold" }}
-                >
-                    {city.name}
-                </Typography>
-                <Typography
-                    variant="h3"
-                    color="white.main"
-                    sx={{ fontWeight: "bold" }}
-                >
-                    {city.country}
-                </Typography>
-            </Stack>
-        </Box>
-    );
+  return (
+    <Box
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      sx={{
+        position: "relative",
+        backgroundImage: `url(${img})`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        minWidth: "100vw",
+        height: "85vh",
+      }}
+    >
+      <Box
+        sx={{
+          position: "absolute",
+          top: "1rem",
+          right: { sm: "1rem" },
+        }}
+      >
+        <GenerateButton
+          list={list}
+          setCity={setCity}
+          setChecked={setChecked}
+          isTryAgain
+        />
+      </Box>
+      <Stack alignItems="center" justifyContent="center">
+        <Typography variant="h1" color="white.main" sx={{ fontWeight: "bold" }}>
+          {city.name}
+        </Typography>
+        <Typography variant="h3" color="white.main" sx={{ fontWeight: "bold" }}>
+          {city.country}
+        </Typography>
+      </Stack>
+    </Box>
+  );
 }
 
 export default CityHeader;
--- src/components/CityWikipediaInfos.jsx
+++ src/components/CityWikipediaInfos.jsx
@@ -3,40 +3,40 @@ import wiki from "wikipedia";
 import React, { useEffect, useState } from "react";
 
 function CityWikipediaInfos({ city }) {
-    const [infos, setInfos] = useState("");
-    const [url, setUrl] = useState("");
+  const [infos, setInfos] = useState("");
+  const [url, setUrl] = useState("");
 
-    const handleWikiParse = async () => {
-        try {
-            const page = await wiki.page(`${city.name}`);
+  const handleWikiParse = async () => {
+    try {
+      const page = await wiki.page(`${city.name}`);
 
-            const summary = await page.summary();
-            setInfos(summary.extract);
-            setUrl(page.fullurl);
-        } catch (error) {
-            console.log(error);
-        }
-    };
+      const summary = await page.summary();
+      setInfos(summary.extract);
+      setUrl(page.fullurl);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    useEffect(() => {
-        handleWikiParse();
-    }, []);
+  useEffect(() => {
+    handleWikiParse();
+  }, []);
 
-    return (
-        <>
-            {infos && (
-                <Box>
-                    <Typography gutterBottom variant="body1">
-                        {infos}
-                    </Typography>
-                    <Link href={url} underline="hover">
-                        See more on Wikipedia...
-                    </Link>
-                    <Divider sx={{ marginTop: "1rem" }} />
-                </Box>
-            )}
-        </>
-    );
+  return (
+    <>
+      {infos && (
+        <Box>
+          <Typography gutterBottom variant="body1">
+            {infos}
+          </Typography>
+          <Link href={url} underline="hover">
+            See more on Wikipedia...
+          </Link>
+          <Divider sx={{ marginTop: "1rem" }} />
+        </Box>
+      )}
+    </>
+  );
 }
 
 export default CityWikipediaInfos;
